aa31d087be7fdf54ab78e026b43a5debc350247a
vaibhavrajpianist <vaibhavrajpianist@gmail.com>
2024-01-20 18:50:42 +0530

    fixed alert error on login/signup

--- src/hooks/useSignupWithEmailandPassword.js
+++ src/hooks/useSignupWithEmailandPassword.js
@@ -1,56 +1,59 @@
-import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth"
-import { auth, firestore } from "../firebase/firebase"
-import { collection, doc, getDocs, query, setDoc, where } from "firebase/firestore"
-import useShowToast from "./useShowToast"
-import useAuthStore from "../store/authStore"
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth, firestore } from "../firebase/firebase";
+import { collection, doc, getDocs, query, setDoc, where } from "firebase/firestore";
+import useShowToast from "./useShowToast";
+import useAuthStore from "../store/authStore";
 
-const useSignupWithEmailandPassword = () => {
-    const [ createUserWithEmailAndPassword, loading, error] = useCreateUserWithEmailAndPassword(auth)
-    const showToast = useShowToast();
-    const loginUser = useAuthStore(state => state.login);
+const useSignUpWithEmailAndPassword = () => {
+	const [createUserWithEmailAndPassword, , loading, error] = useCreateUserWithEmailAndPassword(auth);
+	const showToast = useShowToast();
+	const loginUser = useAuthStore((state) => state.login);
 
-    const signup = async (inputs) => {
-        if (!inputs.email || !inputs.password || !inputs.fullName || !inputs.username) {
-            showToast("Error", "Please fill all the fields", "error")
-            return
-        }
-        const usersRef = collection(firestore, "users");
-        const q = query(usersRef, where("username", "==", inputs.username));
-        const querySnapsnot = await getDocs(q);
+	const signup = async (inputs) => {
+		if (!inputs.email || !inputs.password || !inputs.username || !inputs.fullName) {
+			showToast("Error", "Please fill all the fields", "error");
+			return;
+		}
 
-        if (!querySnapsnot.empty) {
-            showToast("Error", "Username already exists", "error");
-            return
-        }
+		const usersRef = collection(firestore, "users");
 
-        try {
-            const newUser = await createUserWithEmailAndPassword(inputs.email, inputs.password)
-            if (!newUser && error) {
-                showToast("Error", error.message, "error")
-                return
-            }
-            if (newUser) {
-                const userDocument = {
-                    uid: newUser.user.uid,
-                    email: inputs.email,
-                    username: inputs.username,
-                    fullName: inputs.fullName,
-                    bio: "",
-                    profilePicUrl: "",
-                    followers: [],
-                    following: [],
-                    posts: [],
-                    createdAt: Date.now()
-                }
-                await setDoc(doc(firestore, "users", newUser.user.uid), userDocument);
-                localStorage.setItem("user-info", JSON.stringify(userDocument));
-                loginUser(userDocument);
-                }
-        } catch (error) {
-            showToast("Error", error.message, "error")
-        }
-    }
-    return { loading, error, signup }
-}
+		const q = query(usersRef, where("username", "==", inputs.username));
+		const querySnapshot = await getDocs(q);
 
-export default useSignupWithEmailandPassword
+		if (!querySnapshot.empty) {
+			showToast("Error", "Username already exists", "error");
+			return;
+		}
+
+		try {
+			const newUser = await createUserWithEmailAndPassword(inputs.email, inputs.password);
+			if (!newUser && error) {
+				showToast("Error", error.message, "error");
+				return;
+			}
+			if (newUser) {
+				const userDoc = {
+					uid: newUser.user.uid,
+					email: inputs.email,
+					username: inputs.username,
+					fullName: inputs.fullName,
+					bio: "",
+					profilePicURL: "",
+					followers: [],
+					following: [],
+					posts: [],
+					createdAt: Date.now(),
+				};
+				await setDoc(doc(firestore, "users", newUser.user.uid), userDoc);
+				localStorage.setItem("user-info", JSON.stringify(userDoc));
+				loginUser(userDoc);
+			}
+		} catch (error) {
+			showToast("Error", error.message, "error");
+		}
+	};
+
+	return { loading, error, signup };
+};
+
+export default useSignUpWithEmailAndPassword;
